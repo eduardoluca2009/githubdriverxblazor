6b62694b51a0e56b264e2c92706ebbbdd0f3a814
eduardoluca2009 <eduardoluca2009@gmail.com>
2026-08-24 10:00:41 -0400

    driverxblazor

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using DriveXBlazor.Components;
+using DriveXBlazor.Configs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Configuração da Conexão com o Banco de Dados MYSQL
+builder.Services.AddScoped<Conexao>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
